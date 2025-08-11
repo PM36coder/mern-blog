237d18cd666d0b1486290bf65088fcaa5d162770
PM36coder <pravesh63362pk@gmail.com>
2025-08-11 15:14:45 +0530

    fixing some bugs

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,29 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
+import { resolve, dirname } from 'path'
+import { fileURLToPath } from 'url'
 
-// https://vite.dev/config/
+// __dirname ko ESM me define karna
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  base: '/'
+  plugins: [
+    react(),
+    tailwindcss(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: '_redirects',
+          dest: '.' // root of dist
+        }
+      ]
+    })
+  ],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+    },
+  },
 })
